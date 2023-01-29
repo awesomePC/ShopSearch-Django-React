99b222ffb02f1cd024e995e9efe178ab7a1ba4ad
awesome <billycrow268@gmail.com>
2023-01-29 13:52:50 -0500

    pagenation done

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,35 +5,20 @@ from .serializers import SearchHistorySerializer, ItemsSerializer
 from .models import SearchHistory, Items
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-
-
-
-###############
-
 from django.core import files
 from io import BytesIO
 import requests
 import urllib.request
-
-###########################
-import requests
 import json
 import os
 import time
-from threading import Thread
 
-# https://api-gw.onebound.cn/taobao/item_search/?key=t_856 2094008186&secret=20230114&q=dell&start_price=0&end_price=0&page=1&cat=0&discount_only=&sort =&page_size=&seller_info=&nick=&ppath=&imgid=&filter="=
-# url = "https://api-gw.onebound.cn/taobao/item_search_shop/?key=t_856 2094008186&&shop_id=57301367&page=1&sort=&&lang=zh-CN&secret=20230114"
-
-# headers = {
-#     "Accept-Encoding": "gzip",
-#     "Connection": "close"
-# }
+#global variables
 url_list = []
 page_count = 0
 new_search = False
 
-#################################
+########################################################################
 #retrieve search history for dropdown data in the offline mode first page
 @api_view(['GET', 'POST'])
 def search_history(request):
@@ -57,32 +42,54 @@ def search_history(request):
 def online_items(request):
     #delete all database
 
-    items = Items.objects.all()
-    history = SearchHistory.objects.all()
-    for i in items:
-        i.delete()
-    for j in history:
-        j.delete()
+    # items = Items.objects.all()
+    # history = SearchHistory.objects.all()
+    # for i in items:
+    #     i.delete()
+    # for j in history:
+    #     j.delete()
 
-    #end 
+    #end delete all database
 
+    per_page = int(request.query_params.get('per_page'))
+    page_num = int(request.query_params.get('page_num'))
     # key = "t_856 2094008186"
-    key = request.data['key']
+    key = request.query_params.get('key')
     # secret = "20230114"
-    secret = request.data['secret']
+    secret = request.query_params.get('secret')
     # search_keyword = "dell"
-    search_keyword = request.data['keyword']
+    search_keyword = request.query_params.get('search_keyword')
     
-    district = request.data['district']
+    district = request.query_params.get('district')
 
     global page_count, new_search
-  
-    receive_json_and_analyse(key, secret, search_keyword, district, 1)
+    
+    arr_argument = []
+    arr_argument.append(key)
+    arr_argument.append(secret)
+    arr_argument.append(search_keyword)
+    arr_argument.append(district)
+    arr_argument.append(1)
+    receive_json_and_analyse(arr_argument)
+    # receive_json_and_analyse(key, secret, search_keyword, district, 1)
 
-    if page_count != 0:
+    if page_count != 0 and not new_search:
         print("pagenum", page_count)
+        page_list = []
         for page in range(2,page_count):
-            page_count = receive_json_and_analyse(key, secret, search_keyword, district, page)
+            arr_argument = []
+            arr_argument.append(key)
+            arr_argument.append(secret)
+            arr_argument.append(search_keyword)
+            arr_argument.append(district)
+            arr_argument.append(page)
+            page_list.append(arr_argument)
+            
+            page_count = receive_json_and_analyse(arr_argument)
+        
+        # from concurrent.futures import ThreadPoolExecutor
+        # with ThreadPoolExecutor(max_workers=16) as executor:
+        #     executor.map(receive_json_and_analyse, page_list)
 
         # Adding information about user agent
         opener=urllib.request.build_opener()
@@ -93,10 +100,22 @@ def online_items(request):
         with ThreadPoolExecutor(max_workers=16) as executor:
         #executor.map(downloadImage, url_list) #urls=[list of url]
             executor.map(downloadOneImage, url_list) #urls=[list of url]
-
+    
+    #initialize global variables
     page_count = 0
     new_search = False
-    return Response ({"msg": "success"})
+
+    items = Items.objects.filter(District=district, SearchKeyword=search_keyword).order_by('-id')
+    total = items.count()
+    start = per_page*(page_num-1)
+    end = per_page*(page_num-1) + per_page
+    if end > total:
+        end = total
+    items = items[start: end]
+
+    serializer = ItemsSerializer(items, many=True)
+
+    return Response({"data":serializer.data, "total": total})
 
 #in offline mode#
 #according to selected keyword retrieve data from DB 
@@ -104,16 +123,31 @@ def online_items(request):
 def offline_items(request):
     
     if request.method =='GET':
-        # permission_classes = [AllowAny]
+        per_page = int(request.query_params.get('per_page'))
+        page_num = int(request.query_params.get('page_num'))
         # print(request.data['District'])
-        items = Items.objects.all()
-        # if request.data['SearchKeyword'] == "":
-        #     items = Items.objects.all()
-        # else:
-        #     items = Items.objects.filter(District=request.data['District'], SearchKeyword=request.data['SearchKeyword'])
+      
+        if request.query_params.get('search_keyword') == "":
+            items = Items.objects.all().order_by('-id')
+            total = items.count()
+            start = per_page*(page_num-1)
+            end = per_page*(page_num-1) + per_page
+            if end > total:
+                end = total
+            items = items[start: end]
+        else:
+            items = Items.objects.filter(District=request.query_params.get('district'), SearchKeyword=request.query_params.get('search_keyword')).order_by('-id')
+            total = items.count()
+            start = per_page*(page_num-1)
+            end = per_page*(page_num-1) + per_page
+            if end > total:
+                end = total
+            items = items[start: end]
+
         serializer = ItemsSerializer(items, many=True)
-        return Response(serializer.data)
-        # return Response ({"name": "superstar", "data": request.data})
+
+        return Response({"data":serializer.data, "total": total})
+        
     if request.method =='POST':
         serializer = ItemsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -147,7 +181,9 @@ def downloadOneImage(image_url):
     #time.sleep(5)
 
 #donwload json
-def receive_json_and_analyse(key, secret, search_keyword, district, page):
+# def receive_json_and_analyse(key, secret, search_keyword, district, page):
+def receive_json_and_analyse(arr_argument):
+    key, secret, search_keyword, district, page = [element for element in arr_argument]
     print('page', page)
     url = f"https://api-gw.onebound.cn/{district}/item_search/?key={key}&secret={secret}&q={search_keyword}&start_price=0&end_price=0&page={page}&cat=0&discount_only=&sort =&page_size=&seller_info=&nick=&ppath=&imgid=&filter="
 
@@ -173,16 +209,16 @@ def receive_json_and_analyse(key, secret, search_keyword, district, page):
                 print("true")
                 if page == 1:
                     global page_count
-                    page_count = 5#int(json_obj['items']["pagecount"])
+                    page_count = int(json_obj['items']["pagecount"])
+                    new_search_history = SearchHistory(District=district, SearchKeyword=search_keyword)
+                    new_search_history.save()
                 break
             time.sleep(1)
             loop_num += 1
             if loop_num == 5:
                 break
 
-        new_search_history = SearchHistory(District=district, SearchKeyword=search_keyword)
-        new_search_history.save()
-
+        
         for obj in json_obj['items']["item"]:
             if obj["pic_url"] is None:
                 filename = None
@@ -208,8 +244,6 @@ def receive_json_and_analyse(key, secret, search_keyword, district, page):
                             DetailUrl=obj["detail_url"],)
             new_item.save()
     
-            
-
             # from django.core import files
             # from io import BytesIO
             # import requests
@@ -230,7 +264,6 @@ def receive_json_and_analyse(key, secret, search_keyword, district, page):
             #     # file_name = url.split("/")[-1]  # There's probably a better way of doing this but this is just a quick example
             #     new_item.Image.save(img_filename, files.File(fp))
             # time.sleep(2)
-
 
 # Create your views here.
 @api_view(['GET', 'POST'])
